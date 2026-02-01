5831dca6737ffbe9bc4cb96f6749fcab8dfd522d
shafrinahamed0007 <mrdeveloperdevv@gmail.com>
2026-02-01 14:17:35 +0600

    Different types of parameters for a function

---
+++ various-params.js
@@ -0,0 +1,45 @@
+/**
+ * for a given string tell whether it has even number of characters or not
+ *
+ */
+
+function evenSizedString(str) {
+  const size = str.length;
+  console.log(str, size);
+  if (size % 2 === 0) {
+    console.log("Even Size");
+    return true;
+  } else {
+    console.log("Odd Size");
+    return false;
+  }
+}
+
+// evenSizedString("Dhaka");
+// evenSizedString("Noakhali");
+
+function doubleOrTriple(number, doDouble) {
+  if (doDouble === true) {
+    const result = number * 2;
+    return result;
+  } else {
+    const result = number * 3;
+    return result;
+  }
+}
+
+// console.log(doubleOrTriple(5 , true));
+// console.log(doubleOrTriple(5 , false));
+
+function numberOfElements(numbers){
+    const len = numbers.length;
+    return len;
+}
+
+// console.log(numberOfElements([12,78,99,14,63,45]));
+
+function getAge(person){
+    const age = person.age;
+    return age;
+    
+}
