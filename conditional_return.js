b0cab2b494234ade3d96d83f34511134cffda3ec
shafrinahamed0007 <mrdeveloperdevv@gmail.com>
2026-02-01 14:01:34 +0600

    recap and conditional return of odd and even

---
+++ conditional_return.js
@@ -0,0 +1,25 @@
+// check event number
+function isEven(number){
+    if(number%2==0){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+// check odd number
+function isOdd(number){
+    if(number%2==1){
+        return true;
+
+    }
+    return false;
+}
+
+console.log(isEven(5));
+console.log(isEven(6));
+
+console.log(isOdd(10));
+console.log(isOdd(7));
+
+
